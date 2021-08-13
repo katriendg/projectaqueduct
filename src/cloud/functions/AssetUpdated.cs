edff722c3929233a2813f1f494d2454d907b8701
Michiel van Schaik <mivansch@microsoft.com>
2021-08-13 11:44:12 +0200

    Added OperationStatus updates for Region twins.

--- src/cloud/functions/AssetUpdated.cs
+++ src/cloud/functions/AssetUpdated.cs
@@ -28,9 +28,9 @@ namespace ProjectAqueduct.Functions
                 string adtInstanceUrl = "https://" + properties[i]["cloudEvents:source"];
                 var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), credentials);
 
-                // Get and validate the twin from the message
-                string twinId = properties[i]["cloudEvents:subject"];
-                logger.LogInformation($"Patch twin '{twinId}' using: {messages[i]}");
+                // Get and validate the asset twin from the message
+                string assetTwinId = properties[i]["cloudEvents:subject"];
+                logger.LogInformation($"Patch twin '{assetTwinId}' using: {messages[i]}");
                 TwinMessage twinMessage = JsonSerializer.Deserialize<TwinMessage>(messages[i]);
                 if (twinMessage.modelId.StartsWith("dtmi:sample:aqueduct:asset:"))
                 {
@@ -39,7 +39,7 @@ namespace ProjectAqueduct.Functions
                     {
                         if (patch.path == "/FlowCondition")
                         {
-                            await UpdateAreaStatus(client, twinId, patch.value.GetInt32(), logger);
+                            await UpdateAreaStatus(client, assetTwinId, patch.value.GetInt32(), logger);
                             break;
                         }
                     }
@@ -47,20 +47,20 @@ namespace ProjectAqueduct.Functions
             }
         }
 
-        private static async Task UpdateAreaStatus(DigitalTwinsClient client, string twinId, int flowCondition, ILogger logger)
+        private static async Task UpdateAreaStatus(DigitalTwinsClient client, string assetTwinId, int flowCondition, ILogger logger)
         {
             var flowConditions = new List<int>();
             flowConditions.Add(flowCondition);
 
             // Get area of the asset
-            AsyncPageable<BasicRelationship> relationships = client.GetRelationshipsAsync<BasicRelationship>(twinId, "isLocatedIn");
+            AsyncPageable<BasicRelationship> relationships = client.GetRelationshipsAsync<BasicRelationship>(assetTwinId, "isLocatedIn");
             await foreach (BasicRelationship relationship in relationships)
             {
                 string areaTwinId = relationship.TargetId;
-                logger.LogInformation($"Twin '{twinId}' is located in area '{areaTwinId}'");
+                logger.LogInformation($"Twin '{assetTwinId}' is located in area '{areaTwinId}'");
 
-                // Get all assets of the same area with not normal condition
-                string query = $"SELECT asset.$dtId, asset.FlowCondition FROM DIGITALTWINS asset JOIN area RELATED asset.isLocatedIn WHERE area.$dtId='{areaTwinId}' AND asset.$dtId!='{twinId}' AND asset.FlowCondition!=4";
+                // Get all other assets of the same area with not normal condition
+                string query = $"SELECT asset.$dtId, asset.FlowCondition FROM DIGITALTWINS asset JOIN area RELATED asset.isLocatedIn WHERE area.$dtId='{areaTwinId}' AND asset.$dtId!='{assetTwinId}' AND asset.FlowCondition!=4";
                 AsyncPageable<BasicDigitalTwin> assetTwins = client.QueryAsync<BasicDigitalTwin>(query);
                 await foreach (BasicDigitalTwin assetTwin in assetTwins)
                 {
@@ -77,17 +77,16 @@ namespace ProjectAqueduct.Functions
                 logger.LogInformation($"New status of area '{areaTwinId}' is {newAreaStatus}");
 
                 // Get area twin
-                BasicDigitalTwin areaTwin;
                 Response<BasicDigitalTwin> twinResponse = await client.GetDigitalTwinAsync<BasicDigitalTwin>(areaTwinId);
-                areaTwin = twinResponse.Value;
+                BasicDigitalTwin areaTwin = twinResponse.Value;
 
                 // Create patch document to update the area twin
                 bool updateTwin = false;
                 var updateTwinData = new JsonPatchDocument();
-                if (areaTwin.Contents.TryGetValue("OperationalStatus", out object operationalStatusValue))
+                if (areaTwin.Contents.TryGetValue("OperationalStatus", out object areaStatusValue))
                 {
-                    int operationalStatus = ((JsonElement)operationalStatusValue).GetInt32();
-                    if (operationalStatus != newAreaStatus)
+                    int areaStatus = ((JsonElement)areaStatusValue).GetInt32();
+                    if (areaStatus != newAreaStatus)
                     {
                         updateTwin = true;
                         updateTwinData.AppendReplace("/OperationalStatus", newAreaStatus);
@@ -104,6 +103,68 @@ namespace ProjectAqueduct.Functions
                 {
                     await client.UpdateDigitalTwinAsync(areaTwinId, updateTwinData);
                     logger.LogInformation($"Patch twin '{areaTwinId}' using {updateTwinData.ToString()}");
+
+                    await UpdateRegionStatus(client, areaTwinId, newAreaStatus, logger);
+                }
+            }
+        }
+
+        private static async Task UpdateRegionStatus(DigitalTwinsClient client, string areaTwinId, int operationalStatus, ILogger logger)
+        {
+            var operationalStatuses = new List<int>();
+            operationalStatuses.Add(operationalStatus);
+
+            // Get region of the area
+            AsyncPageable<BasicRelationship> relationships = client.GetRelationshipsAsync<BasicRelationship>(areaTwinId, "isLocatedIn");
+            await foreach (BasicRelationship relationship in relationships)
+            {
+                string regionTwinId = relationship.TargetId;
+                logger.LogInformation($"Twin '{areaTwinId}' is located in region '{regionTwinId}'");
+
+                // Get all other areas of the same region
+                string query = $"SELECT area.$dtId, area.OperationalStatus FROM DIGITALTWINS area JOIN region RELATED area.isLocatedIn WHERE region.$dtId='{regionTwinId}' AND area.$dtId!='{areaTwinId}'";
+                AsyncPageable<BasicDigitalTwin> areaTwins = client.QueryAsync<BasicDigitalTwin>(query);
+                await foreach (BasicDigitalTwin areaTwin in areaTwins)
+                {
+                    if (areaTwin.Contents.TryGetValue("OperationalStatus", out object areaStatusValue))
+                    {
+                        int areaStatus = ((JsonElement)areaStatusValue).GetInt32();
+                        logger.LogInformation($"Area '{areaTwin.Id}' in same region '{regionTwinId}' has operational status {areaStatus}");
+                        operationalStatuses.Add(areaStatus);
+                    }                
+                }
+
+                // Update the region status
+                int newRegionStatus = GetRegionStatus(operationalStatuses);
+                logger.LogInformation($"New status of region '{regionTwinId}' is {newRegionStatus}");
+
+                // Get region twin
+                Response<BasicDigitalTwin> twinResponse = await client.GetDigitalTwinAsync<BasicDigitalTwin>(regionTwinId);
+                BasicDigitalTwin regionTwin = twinResponse.Value;
+
+                // Create patch document to update the region twin
+                bool updateTwin = false;
+                var updateTwinData = new JsonPatchDocument();
+                if (regionTwin.Contents.TryGetValue("OperationalStatus", out object regionStatusValue))
+                {
+                    int regionStatus = ((JsonElement)regionStatusValue).GetInt32();
+                    if (regionStatus != newRegionStatus)
+                    {
+                        updateTwin = true;
+                        updateTwinData.AppendReplace("/OperationalStatus", newRegionStatus);
+                    }
+                }
+                else
+                {
+                    updateTwin = true;
+                    updateTwinData.AppendAdd("/OperationalStatus", newRegionStatus);
+                }
+
+                // Update the region twin
+                if (updateTwin)
+                {
+                    await client.UpdateDigitalTwinAsync(regionTwinId, updateTwinData);
+                    logger.LogInformation($"Patch twin '{regionTwinId}' using {updateTwinData.ToString()}");
                 }
             }
         }
@@ -126,6 +187,17 @@ namespace ProjectAqueduct.Functions
                 };
                 if (conditionStatus > newStatus)
                     newStatus = conditionStatus;
+            }
+            return newStatus;
+        }
+
+        private static int GetRegionStatus(IList<int> statuses)
+        {
+            int newStatus = 0;
+            foreach(int status in statuses)
+            {
+                if (status > newStatus)
+                    newStatus = status;
             }
             return newStatus;
         }
